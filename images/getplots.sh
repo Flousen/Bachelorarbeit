28c71309f458a5f11b58bbf0b28f82ba7163b5da
Florian Krötz <florian.kroetz@uni-ulm.de>
2018-08-12 19:36:00 +0200

    pres

--- images/getplots.sh
+++ images/getplots.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-scp floriank@thales.mathematik.uni-ulm.de:Bachelorarbeit/src/hpc/bench/blk.png .
-scp floriank@thales.mathematik.uni-ulm.de:Bachelorarbeit/src/hpc/bench/unblk.png .
+#scp floriank@thales.mathematik.uni-ulm.de:Bachelorarbeit/src/hpc/bench/img/blk.png .
+#scp floriank@thales.mathematik.uni-ulm.de:Bachelorarbeit/src/hpc/bench/img/unblk.png .
+scp floriank@thales.mathematik.uni-ulm.de:Bachelorarbeit/src/hpc/bench/img/*.png .
